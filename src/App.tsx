053809e10869885f9b9c95c185dd180a66f21589
Maxim Tynyanko <m.tynyanko@tg.dunice.net>
2024-08-19 17:56:19 +0300

    header component in process

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import Button from '@mui/material/Button';
+import Header from './header.tsx';
+
+
 const App = () => {
+  let count = 0;
   return (
     <>
+    <Header />
       <p>hello world</p>
+      <Button variant='contained' onClick={()=>{
+        count++;
+        console.log(count);
+      }}>button</Button>
     </>
   )
 }
